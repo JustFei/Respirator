3bf0c40f55cc8be2cb533946ecce0c0e340d00ec
Faith <kaixiaofei@gmail.com>
2017-07-19 09:31:06 +0800

    Initial Commit

---
+++ Respirator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Respirator
+//
+//  Created by JustFei on 2017/7/19.
+//  Copyright © 2017年 manridy.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
